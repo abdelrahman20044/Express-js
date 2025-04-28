a86f7c19511e479b63712195d7f6e2ffa668591f
abdelrahman <abdelrahman.elmansy23@gmail.com>
2025-04-28 22:53:43 +0300

    query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -55,7 +55,12 @@ const tourSchema = new mongoose.Schema(
       select: false, // not shown for the user
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
+
   {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
@@ -70,11 +75,22 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-tourSchema.post('save', function (doc, next) {
-  console.log(doc); // the saved document
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc); // the saved document
+//   next();
+// });
+
+tourSchema.pre(/^find/, function (next) {
+  // /^find/  means any query start with find like find findMany findOne
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
   next();
 });
-
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  // console.log(docs);
+  next();
+});
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
